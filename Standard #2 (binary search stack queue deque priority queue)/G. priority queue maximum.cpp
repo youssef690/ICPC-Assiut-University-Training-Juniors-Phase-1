7325ff90a9b9e512676c7c7c90368b1c8c31eacd
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-05 15:35:11 +0200

    Create G. priority queue maximum.cpp

---
+++ Standard #2 (binary search stack queue deque priority queue)/G. priority queue maximum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define loop(i,x) for(int i=0 ; i<x ; i++)
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+	int q; cin >> q;
+	priority_queue<int>s;
+	while (q--)
+	{
+		string z; cin >> z;
+		
+		if (z == "top") cout << s.top() << '\n';
+		else if (z == "pop") s.pop();
+		else
+		{
+			int x; cin >> x;
+			s.push(x);
+		}
+	}
+}
