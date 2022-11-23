03ea993a2c419ea5b735be89201b465740773884
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-23 20:52:43 +0200

    Update J. Next Element.cpp

--- Standard #2 (binary search stack queue deque priority queue)/J. Next Element.cpp
+++ Standard #2 (binary search stack queue deque priority queue)/J. Next Element.cpp
@@ -1,1 +1,29 @@
-//soon
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    int n; cin >> n;
+    stack<pair<int,int>>st; map<int,int>mp;
+
+    for(int i=0 ; i<n ; i++)
+    {
+        int x; cin >> x;
+        while(!st.empty() && x>st.top().first)
+        {
+            mp[st.top().second]=i+1;
+            st.pop();
+        }
+        st.push({x,i});
+    }
+    int q; cin >> q;
+    while(q--)
+    {
+        int val; cin >> val; 
+        if(mp.find(val-1)==mp.end()) cout << -1 << '\n';
+        else cout << mp[val-1] << '\n';
+    }
+}
