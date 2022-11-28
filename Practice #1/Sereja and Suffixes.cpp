79241afbdf5389ab5c51ad9d29347d46ab03202e
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-28 15:12:41 +0200

    Update Sereja and Suffixes.cpp

--- Practice #1/Sereja and Suffixes.cpp
+++ Practice #1/Sereja and Suffixes.cpp
@@ -1,1 +1,28 @@
-soon
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n,m ; cin >> n >> m;
+    vector<int>v(n); for(int i=0 ; i<n ; i++) cin >> v[i];
+    map<int,int>mp,xp;
+    for(int i=n-1 ; i>=0 ; i--)
+    {
+        mp[v[i]]++;
+        if(mp[v[i]]>1) xp[i]=0;
+        else xp[i]=1;
+    }
+    vector<int>suf(n);
+    suf[n-1]=xp[n-1];
+    for(int i=n-2 ; i>=0 ; i--)
+    {
+          suf[i] = suf[i+1]+xp[i];
+    }
+
+    while(m--)
+    {
+        int x; cin >> x;
+        cout << suf[x-1] << '\n';
+    }
+}
