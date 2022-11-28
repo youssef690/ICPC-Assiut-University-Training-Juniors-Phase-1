1a1e8dc74d84b79e0410999a6c1afcff56366f5f
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-11-28 15:11:53 +0200

    Update Andryusha and Socks.cpp

--- Practice #1/Andryusha and Socks.cpp
+++ Practice #1/Andryusha and Socks.cpp
@@ -1,1 +1,26 @@
-soon
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n; cin >> n; n*=2;
+    map<int,int>mp;
+    int res=1,cntr=0;
+
+    while(n--)
+    {
+        int x; cin >> x;
+        if(mp[x]==0)
+        {
+            mp[x]++;
+            cntr++;
+        }
+        else
+        {
+            res = max(res, cntr);
+            cntr--;
+        }
+    }
+    cout << res << '\n';
+}
