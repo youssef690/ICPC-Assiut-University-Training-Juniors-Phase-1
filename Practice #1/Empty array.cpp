bfe1d23f0c32cbdc3331d2742968d78e51448ff9
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-12-03 19:08:10 +0200

    Update Empty array.cpp

--- Practice #1/Empty array.cpp
+++ Practice #1/Empty array.cpp
@@ -1,1 +1,37 @@
-soon
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse2")
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+   ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+   int n; cin >> n;
+   vector<int>v(n), freq(200005);
+   int mx = -1, maxnum=0, sm=INT_MAX;
+   for(int i=0 ; i<n  ;i++)
+   {
+       cin >> v[i];freq[v[i]]++;
+       mx = max(mx,freq[v[i]]);
+       maxnum=max(maxnum, v[i]);
+       sm=min(sm, v[i]);
+   }
+
+
+   while(mx)
+   {
+       for(int i=sm ; i<=maxnum ; i++)
+       {
+           if(freq[i]>0)
+           {
+               cout << i << " ";
+               freq[i]--;
+           }
+       }
+       cout << '\n';
+       mx--;
+
+   }
+}
