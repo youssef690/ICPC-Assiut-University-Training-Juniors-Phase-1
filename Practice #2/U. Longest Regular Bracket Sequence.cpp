91368600c7d0291a5d99381b30f43c774718467a
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2023-02-04 23:25:03 +0200

    Create U. Longest Regular Bracket Sequence.cpp

---
+++ Practice #2/U. Longest Regular Bracket Sequence.cpp
@@ -0,0 +1,69 @@
+/*
+██╗  ██╗ █████╗ ██████╗ ███████╗███████╗███╗   ███╗        ██╗  ██╗██╗  ██╗ █████╗ ██╗     ███████╗██████╗
+██║ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗ ████║        ██║ ██╔╝██║  ██║██╔══██╗██║     ██╔════╝██╔══██╗
+█████╔╝ ███████║██████╔╝█████╗  █████╗  ██╔████╔██║        █████╔╝ ███████║███████║██║     █████╗  ██║  ██║
+██╔═██╗ ██╔══██║██╔══██╗██╔══╝  ██╔══╝  ██║╚██╔╝██║        ██╔═██╗ ██╔══██║██╔══██║██║     ██╔══╝  ██║  ██║
+██║  ██╗██║  ██║██║  ██║███████╗███████╗██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║██║  ██║███████╗███████╗██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
+Believe in yourself ,( try and try and then try ), You can do it
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#include<unordered_map>
+#define endl "\n"
+#define Ceil(x,y) ((x+y-1)/y)
+#define clr(arr, x) memset(arr, x, sizeof arr)
+#define all(v) v.begin(),v.end()
+#define allr(s) s.rbegin(),s.rend()
+#define rt(s) return cout<<s,0
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+#define sz(s)	(int)(s.size())
+#define OO 0x3f3f3f3f3f3f3f3fLL
+const int oo = 0x3f3f3f3f, mod = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+int dx[] {0, 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy[] {0, 0, 0, 1, -1, 1, -1, 1, -1 };
+void K_K(){
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif  !ONLINEJUDGE
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+bool open(char ch){
+	return ch == '(' || ch == '{' || ch == '<' || ch == '[';
+}
+char rev(char ch){
+	if (ch == ')')
+		return '(';
+	if (ch == '}')
+		return '{';
+	if (ch == ']')
+		return '[';
+	if (ch == '>')
+		return '<';
+}
+int main(){
+	K_K();
+	string s; cin >> s;
+	vector<int>tmp(sz(s)); stack<int> st;
+	int cnt = 1, mx = 0;
+	for (int i = 0; i < sz(s); i++) {
+		if (open(s[i]))
+			st.push(i);
+		else if (sz(st) && s[st.top()] == rev(s[i])) {
+			tmp[i] = i - st.top() + 1;
+			if (st.top())
+				tmp[i] += tmp[st.top() - 1];
+			if (tmp[i] > mx)
+				mx = tmp[i], cnt = 1;
+			else if (tmp[i] == mx)
+				cnt++;
+			st.pop();
+		}
+		else 
+           st.push(i);
+	}
+	cout << mx << ' ' << cnt;
+}
